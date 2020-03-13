dada0264d2150c321ffcab4571d56c897bfe0ce8
Tom Sharkey <tbsharkey@gmail.com>
2020-03-13 05:45:01 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,6 @@
 class Dog
-def initialize(name, breed="Mutt")
-  @name = name
-  @breed= breed
-end
-def name
-  @name
-end
-def breed
-  @breed = breed
-end
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed
+  end
 end
